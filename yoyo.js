b413fdc7106477f25cdeb93ad76c7eeac05f48bd
anuragbg <anurag1998eng@gmail.com>
2024-11-16 03:17:09 +0530

    qr- generator in yoyo.js file added

--- yoyo.js
+++ yoyo.js
@@ -1,7 +1,28 @@
-import superheroes from 'superheroes'
+// import superheroes from 'superheroes'
+// var rand=Math.floor(Math.random() * superheroes.length);
+// var sname = superheroes[rand];
+// console.log(`My name is ${sname}`);
 
-var rand=Math.floor(Math.random() * superheroes.length);
 
-var sname = superheroes[rand];
+import inquirer from 'inquirer';
+import qr from 'qr-image'
+import fs from 'fs';
 
-console.log(`My name is ${sname}`);
+inquirer
+  .prompt([{
+    message: "Type your URL: ",
+    name: "URL",
+  },
+])
+  .then((answers) => {
+    const url=answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
